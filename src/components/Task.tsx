75211d9bbdc9738d5a6cb3168d11235f19cbddbc
Mitra Nami <mnami@rossvideo.com>
2022-09-05 21:10:41 -0400

    Add the statuses dropDown to Task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,29 @@
-import { Task as TaskType } from "./types";
+import React from 'react';
+import { Task as TaskType, EditTaskData } from "./types";
+import { TASK_STATUSES } from './Taskspage';
 
 const Task = (props: TaskProps) => {
+
+  const handleStatusChange = (evt: React.ChangeEvent<HTMLSelectElement>) => {
+    props.onStatusChange({id: props.task.id, status: evt.target.value});
+  };
 
   return (
     <div className="task">
       <div className="task-header">
         <div>{props.task.title}</div>
+        <select value={props.task.status} onChange={handleStatusChange}>
+          {
+            TASK_STATUSES.map(status => 
+            <option 
+              key={status}
+              value={status}
+            >
+              {status}
+            </option>
+            )
+          }
+        </select>
       </div>
       <hr />
       <div className="task-body">{props.task.description}</div>
@@ -14,7 +32,8 @@ const Task = (props: TaskProps) => {
 };
 
 type TaskProps = {
-  task: TaskType
+  task: TaskType;
+  onStatusChange: (updatedData: EditTaskData) => void;
 };
 
 export default Task;
